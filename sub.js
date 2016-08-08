f03feb8a0f0a2654476507be67993b76fef0be8a
Victor Ionescu <Victor.Ionescu@msg-systems.com>
2016-08-08 14:16:05 +0300

    2 pub/sub services. 1 manifest.yml

---
+++ sub.js
@@ -0,0 +1,56 @@
+
+var AMQP_SERVICE = "amqp";
+
+// Bring in the module providing the wrapper for cf env
+var cfenvWr = require('./cfenv-wrapper');
+// http server
+var http = require('http');
+
+// SetUp
+
+// get the cloud foundry environment variables
+var appEnv = cfenvWr.getAppEnv();
+var port = appEnv.port;
+var appURL = appEnv.url;
+
+var amqpURL = appEnv.getServiceURL('amqp');
+
+console.log('found URL: %s', amqpURL);
+console.log('running on port %s', port);
+console.log('running service URL: %s', appURL);
+
+
+// HTTP Server -- Start
+
+var server = http.createServer(function (request, response) {
+    response.end('Hello World');
+});
+
+server.listen(port, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: %s", appURL);
+});
+
+
+// Rabbit MQ setup
+var connOpt = {};
+var context = require('rabbit.js').createContext(amqpURL, connOpt);
+
+context.on('ready', function() {
+    console.log('Connected to the Rabbit!');
+    var sub = context.socket('SUB');
+    sub.pipe(process.stdout);
+    sub.connect('messages', function(){
+        console.log('Connected to topic [messages]');
+    });
+});
+
+context.on('error', function(error){
+    console.log('Some error while trying to connect to the Rabbit. %s', error);
+});
+
+context.on('close', function(error){
+    console.log('connection closed');
+});
+
+
